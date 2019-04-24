5abb16f6f48181f380f7fe077cba59b89ba6b021
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-24 11:58:06 +0900

    いろいろ初期設定

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
-import './plugins/element.js';
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import "./registerServiceWorker"
+import "./plugins/element.js"
 
-Vue.config.productionTip = false;
+import Amplify from "aws-amplify"
+import awsExports from "@/aws-exports.js"
+Amplify.configure(awsExports)
+
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { far } from "@fortawesome/free-regular-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+library.add(fas, far, fab)
+Vue.component("font-awesome-icon", FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
